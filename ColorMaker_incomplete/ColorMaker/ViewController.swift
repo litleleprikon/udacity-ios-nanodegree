61ee6ce343ba7218a5772ecc4e648511a1a344ed
Emil Sharifullin <litleleprikon@gmail.com>
2016-06-19 20:05:36 +0300

    [CF] In ColorMaker project Switches changed to Sliders

---
+++ ColorMaker_incomplete/ColorMaker/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  ColorMaker
+//
+//  Created by Jason Schatz on 11/2/14.
+//  Copyright (c) 2014 Udacity. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redControl: UISlider!
+    @IBOutlet weak var greenControl: UISlider!
+    @IBOutlet weak var blueControl: UISlider!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Set colorView on launch
+        changeColorComponent()
+    }
+    
+    @IBAction func changeColorComponent() {
+        
+        // Make sure the program doesn't crash if the controls aren't connected
+        if self.redControl == nil {
+            return
+        }
+        
+        let r: CGFloat = CGFloat(self.redControl.value)
+        let g: CGFloat = CGFloat(self.greenControl.value)
+        let b: CGFloat = CGFloat(self.blueControl.value)
+                
+        colorView.backgroundColor = UIColor(red: r, green: g, blue: b, alpha: 1)
+    }
+}
+
